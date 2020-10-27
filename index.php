bd050805a6b0b79a490c6f7f030992c5f7e72ef5
Michael Weisinger <impozzo@verizon.net>
2020-10-27 15:37:23 -0400

    Intro Test

---
+++ index.php
@@ -0,0 +1,18 @@
+<?PHP 
+$heading1_text = "This is CSC226-GroupProject.";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>CSC226-GroupProject</title>
+</head>
+<body>
+<h1><?PHP 
+echo($heading1_text);
+?></h1>
+<p>Now what?</p>    
+</body>
+</html>
